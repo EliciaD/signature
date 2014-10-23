cdda2f1f405023fac7b244ba0b23211391f90fc2
Elicia Durtnall <n00618011@l209-n-013.mediastudies.ca>
2014-10-22 20:19:16 -0400

    IOS Signature
    Save & Reset
    
    Add save to parse for helpr app

---
+++ DrawPad/ViewController.h
@@ -0,0 +1,35 @@
+//
+//  ViewController.h
+//  DrawPad
+//
+//  Created by Ray Wenderlich on 9/3/12.
+//  Copyright (c) 2012 Ray Wenderlich. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface ViewController : UIViewController <UIActionSheetDelegate> {
+    
+    CGPoint lastPoint;
+    CGFloat red;
+    CGFloat green;
+    CGFloat blue;
+    CGFloat brush;
+    CGFloat opacity;
+    BOOL mouseSwiped;
+    
+}
+
+@property (weak, nonatomic) IBOutlet UIImageView *mainImage;
+@property (weak, nonatomic) IBOutlet UIImageView *tempDrawImage;
+@property (weak, nonatomic) IBOutlet UIButton *reset;
+@property (weak, nonatomic) IBOutlet UIButton *save;
+
+
+
+- (IBAction)reset:(id)sender;
+
+- (IBAction)save:(id)sender;
+
+@end
